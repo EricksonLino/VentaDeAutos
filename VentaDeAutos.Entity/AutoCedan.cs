74e2fa5d67ccc84d5a611cc405505cf29f80eef7
Erickson L <linobarrantes@gmail.com>
2023-05-03 13:32:37 -0500

    Add project files.

---
+++ VentaDeAutos.Entity/AutoCedan.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VentaDeAutos.Entity
+{
+    public class AutoCedan
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Marca { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Modelo { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Transmición { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string TipoDeFrenos { get; set; }
+        [Required]
+        [StringLength(30)]
+        public string Capacidad { get; set; }
+        [Required]
+        [StringLength(50)]
+        public int Stock { get; set; }
+
+    }
+}
