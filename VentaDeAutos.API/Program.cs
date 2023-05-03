d476b6410a769b7247a5e392de14e029b1562ee6
Erickson L <linobarrantes@gmail.com>
2023-05-03 17:06:20 -0500

    inyectamos Dbcontex en program, creamos la Migration y empujamos la migration a la base de datos.

--- VentaDeAutos.API/Program.cs
+++ VentaDeAutos.API/Program.cs
@@ -1,6 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using VentaDeAutos.Repository;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(conf => 
+                                conf.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
---
+++ VentaDeAutos.Repository/Migrations/20230503214702_MigrationInitial.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VentaDeAutos.Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class MigrationInitial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AutoCedans",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Marca = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    color = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    Modelo = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Transmición = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    TipoDeFrenos = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Capacidad = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    Stock = table.Column<int>(type: "int", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AutoCedans", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AutoCedans");
+        }
+    }
+}
