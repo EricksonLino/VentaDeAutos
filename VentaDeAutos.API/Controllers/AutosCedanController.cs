4f3fe184cad6ab627ea3aeb49534e928d3f2fcc7
Erickson L <linobarrantes@gmail.com>
2023-05-03 19:20:23 -0500

    creamos el controlador y listamos, tambien  realizamos la inyeccion en program de application y repository

---
+++ VentaDeAutos.API/Controllers/AutosCedanController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using VentaDeAutos.Application.Interfaces;
+using VentaDeAutos.Entity;
+
+namespace VentaDeAutos.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AutosCedanController : ControllerBase
+    {
+        private readonly IAutoCedanApplication autoCedanApplication;
+
+        public AutosCedanController(IAutoCedanApplication autoCedanApplication)
+        {
+            this.autoCedanApplication = autoCedanApplication;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<AutoCedan>>> ListarAutos()
+        {
+            var autoscedan = await this.autoCedanApplication.ListarAutos();
+            return autoscedan;
+        }
+
+    }
+}
--- VentaDeAutos.API/Program.cs
+++ VentaDeAutos.API/Program.cs
@@ -1,11 +1,17 @@
 using Microsoft.EntityFrameworkCore;
+using VentaDeAutos.Application.Implementaciones;
+using VentaDeAutos.Application.Interfaces;
 using VentaDeAutos.Repository;
+using VentaDeAutos.Repository.Implementaciones;
+using VentaDeAutos.Repository.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddDbContext<ApplicationDbContext>(conf => 
                                 conf.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<IAutoCedanRepository, AutoCedanRepository>();
+builder.Services.AddScoped<IAutoCedanApplication, AutoCedanApplication>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
